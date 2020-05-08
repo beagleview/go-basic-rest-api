130ff71230a2027584e8905e89022cd9fd8a8a9b
taechitc <beagleview@outlook.com>
2020-05-08 14:29:53 +0700

    create article service

---
+++ handles/articleService.go
@@ -0,0 +1,25 @@
+package handles
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func getAllArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Test Content 1"},
+		Article{Title: "Newbie Title", Desc: "Newbie Description", Content: "Newbie and Noob Content"},
+	}
+
+	fmt.Println("Endpoint Hit : Get all articles")
+	json.NewEncoder(w).Encode(articles)
+}
