301521de223e383c6541693ab07f81b7f962df69
taechitc <beagleview@outlook.com>
2020-05-08 14:29:37 +0700

    refactor main to call handlers

--- main.go
+++ main.go
@@ -1,38 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"basic-rest/handles"
 	"log"
 	"net/http"
 )
 
-type Article struct {
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-type Articles []Article
-
-func getAllArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Test Title", Desc: "Test Description", Content: "Test Content 1"},
-		Article{Title: "Newbie Title", Desc: "Newbie Description", Content: "Newbie and Noob Content"},
-	}
-
-	fmt.Println("Endpoint Hit : Get all articles")
-	json.NewEncoder(w).Encode(articles)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Newbie and Noob")
-}
-
 func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", getAllArticles)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Start Service")
+	router := handles.Router()
+
+	log.Print("The service is ready to listen and serve.")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func main() {
