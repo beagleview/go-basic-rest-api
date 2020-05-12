a705260ec0065423ada6be3fc351ca9cb61cc6e1
taechitc <beagleview@outlook.com>
2020-05-12 23:23:49 +0700

    update Graceful shutdown

--- main.go
+++ main.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"basic-rest/handles"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func handleRequest() {
 	log.Println("Start Service")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
 	router := handles.Router()
 
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8080", router))
+
+	killSignal := <-stop
+	switch killSignal {
+	case os.Interrupt:
+		log.Print("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Print("Got SIGTERM...")
+	}
+
+	log.Print("The service is shutting down...")
+	srv.Shutdown(context.Background())
+	log.Print("Server gracefully stopped")
 }
 
 func main() {
